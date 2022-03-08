eaa93af5a25363a41ad5fdc51adda277b241b172
martindev446 <martin.dev.446@gmail.com>
2022-03-07 21:54:49 -0500

    basic conanfile

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from unicodedata import name
+from conans import ConanFile, CMake
+
+class TeamPointsCalc(ConanFile):
+    name = "team-points-calculator"
+    version = "1.0.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = (
+        "gtest/1.11.0",
+        "nlohmann_json/3.10.5",
+    )
+    generators = "cmake", "cmake_find_package"
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
